3e30626e4ce88aa76c24a1f70e9129f7b4cb8027
Katharina Endner <k.endner@gmail.com>
2023-04-06 14:48:15 +0200

    task finished

--- Introducing_JavaScript/Functions_1/script.js
+++ Introducing_JavaScript/Functions_1/script.js
@@ -1,5 +1,29 @@
+//Task 1
+
 function welcomeMsg(name) {
   return `Welcome ${name}!`;
 }
 
 console.log(welcomeMsg("Dobby"));
+
+//Task2
+
+function calcGrossPrice(price, taxrate) {
+  return price + price * taxrate;
+}
+
+console.log(calcGrossPrice(20, 0.19));
+
+console.log(calcGrossPrice(40, 0.16));
+
+//Task 3
+
+function addPositive(num1, num2) {
+  return Math.abs(num1) + Math.abs(num2);
+}
+
+console.log(addPositive(2, 3));
+
+console.log(addPositive(3, -5));
+
+console.log(addPositive(-1, -8));
