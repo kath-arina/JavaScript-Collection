52a6ce4b28fa4029ea99cccf52d633173cb5c17c
Katharina Endner <k.endner@gmail.com>
2023-04-06 10:00:45 +0200

    initial commit

---
+++ Introducing_JavaScript/Hallo_World/script.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+console.log(567.7 / 34);
+console.log('Hallo"Null"', 2 - 2);
+console.log(`dummesBrot`, 1 + 3 / 2);
+console.log(!(2 === 3 && 2 < 3));
+console.log(5 + 5 === 10);
+console.log("Hallo" + "ICH" + "Brot" + 6);
